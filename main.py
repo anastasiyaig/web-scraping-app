f80808d254e11e6e7af20ebe870661fc5eb346ac
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-07-04 11:55:13 +0300

    feat: add email sending function
    - email sending function added
    - script is running in the loop (checking the web site each 2 seconds)

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import time
+
+import certifi
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+HEADERS = \
+    {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+    }
+SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL')
+EMAIL_PASSWORD = os.getenv("GMAIL_PASSWORD")
+EMAIL_SUBJECT = "New event found!"
+
+SCRAPING_PATTERN = "No upcoming tours"
 
 
 def scrape(url):
@@ -19,8 +33,20 @@ def extract(source):
     return value
 
 
-def send_email():
-    print("Email was sent")
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context(cafile=certifi.where())
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(
+            user=SENDER_EMAIL,
+            password=EMAIL_PASSWORD
+        )
+        server.sendmail(
+            from_addr=SENDER_EMAIL,
+            to_addrs=RECEIVER_EMAIL,
+            msg=message)
 
 
 def store(data):
@@ -34,12 +60,17 @@ def read(data):
 
 
 if __name__ == "__main__":
-    scraped = scrape(url=URL)
-    extracted = extract(source=scraped)
-    print(extracted)
-    content = read(data=extracted)
+    while True:
 
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(data=extracted)
-            send_email()
+        scraped = scrape(url=URL)
+        extracted = extract(source=scraped)
+        print(extracted)
+        content = read(data=extracted)
+
+        if extracted != SCRAPING_PATTERN:
+            if extracted not in content:
+                store(data=extracted)
+                body = f"Subject: {EMAIL_SUBJECT}" + "\n" + extracted
+                composed_email = str(body).encode("utf-8")
+                send_email(composed_email)
+        time.sleep(2)
